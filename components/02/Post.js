d50d996cbbef45ad974c5730fb5babb1fae4c6e1
Ma Diallo <saylujalloh@gmail.com>
2021-03-01 16:40:46 -0500

    post button added

--- components/02/Post.js
+++ components/02/Post.js
@@ -71,63 +71,97 @@ class Post extends Component{
     } else {
        
       return (
-          
-          <View style={styles.container}>
-              <View>
-                  <Image source={{uri: `${this.state.uri}`}}  style={{ width: 400, height: 40 }} />
-              </View>
-            <Camera style={{ flex: 1 }} type={this.state.cameraType}  ref={ref => {this.camera = ref}} autoFocus="on">
-              <View style={{flex:1, flexDirection:"row",justifyContent:"space-between",margin:30}}>
-                <TouchableOpacity
-                  style={{
-                    alignSelf: 'flex-end',
-                    alignItems: 'center',
-                    backgroundColor: 'transparent'                 
-                  }}
-                  onPress={()=>this.pickImage()}>
-                  <Ionicons
-                      name="ios-photos" size={30} color="white"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{
-                    alignSelf: 'flex-end',
-                    alignItems: 'center',
-                    backgroundColor: 'transparent',
-                  }}
-                  onPress={()=>this.takePicture()}
-                  >
-                  <Ionicons
-                      name="ios-camera" size={35} color="white"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{
-                    alignSelf: 'flex-end',
-                    alignItems: 'center',
-                    backgroundColor: 'transparent',
-                  }}
-                  onPress={()=>this.handleCameraType()}
-                  >
-                  <Ionicons
-                      name="ios-reverse-camera" size={35} color="white"
-                  />
-                </TouchableOpacity>
-              </View>
-            </Camera>
+        <View style={styles.container}>
+        <View>
+            {/* <Image source={{uri: `${this.state.uri}`}}  style={{ width: 400, height: 40 }} /> */}
+            <View style={styles.header}>
+             <TouchableOpacity style={styles.canselBtn}>
+               <Text style={styles.canselText}>Cansel</Text>
+             </TouchableOpacity>
+             <TouchableOpacity style={styles.nextBtn} disabled={true}>
+               <Text style={styles.nextText}>Next</Text>
+             </TouchableOpacity>
+            </View>
         </View>
+      <Camera style={{ flex: 1 }} type={this.state.cameraType}  ref={ref => {this.camera = ref}} autoFocus="on">
+        
+        <View style={{flex:1, flexDirection:"column",margin:20}}>
+          <View style={{flex:6, flexDirection:"row", justifyContent:"center"}} >
+          <TouchableOpacity  style={styles.camera} onPress={()=>this.takePicture()} >
+              <Ionicons name="ios-camera" size={50} color="white" />
+            </TouchableOpacity>
+          </View>
+            <View style={{flex:1, flexDirection:"row",justifyContent:"space-between"}}>  
+              <TouchableOpacity style={styles.btn} onPress={()=>this.pickImage()}>
+                <Ionicons name="ios-photos" size={30} color="white" />
+              </TouchableOpacity>
+
+              { this.state.image.length != 0?
+              <TouchableOpacity style={styles.takenImg}>
+                <Image source={{uri: `${this.state.image}`}}  style={{ width: 50, height: 45 }} />
+              </TouchableOpacity>: <></>
+              }
+              <TouchableOpacity style={styles.btn} onPress={()=>this.handleCameraType()}>
+                <Ionicons name="ios-reverse-camera" size={35} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Camera>
+    </View>
       );
     }
     }
 }
 
-
-
-
 const styles =  StyleSheet.create({
-    container:{
-        flex: 1,
-        marginTop: Constants.statusBarHeight,
-    }
+  container:{
+      flex: 1,
+      marginTop: Constants.statusBarHeight,
+      backgroundColor: '#000'
+  },
+  header:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 2,
+    borderBottomColor: '#fff',
+    backgroundColor: '#e0e0e0'
+  },
+  canselBtn:{
+    padding: 10,
+    borderRadius: 10
+  },
+  nextBtn:{
+    padding: 10,
+    borderRadius: 10
+  },
+  canselText:{
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  nextText:{
+    fontSize: 18,
+    fontWeight: '600'
+  
+  },
+  camera:{
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'transparent' 
+  },
+  takenImg:{
+    backgroundColor: 'red',
+    height: 50,
+    width: 40,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  btn:{
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'transparent' 
+  }
+
 })
+
 export default Post;
