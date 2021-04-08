7a24a5c3efd9bef333c894e57b0661849e77c8f3
jalloh-codes <saylujalloh@gmail.com>
2021-04-08 18:19:07 -0400

    ignore keys file

--- keys.js
+++
@@ -1,7 +0,0 @@
-export const aws = {
-    // keyPrefix: "images/",
-    bucket: 'gsa-image-store',
-    region: 'us-east-2',
-    accessKey: 'AKIAVEQU6EBCR5I3O3OM',
-    secretKey: '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',
-}
