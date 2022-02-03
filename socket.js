7e975a532b8944ee8d1d7231eb607797057d97df
jalloh-codes <saylujalloh@gmail.com>
2022-02-03 00:23:12 -0500

    add link

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io('http://localhost:8080', {
+export const socket = io('https://gsabackend.herokuapp.com/graphql', {
     autoConnect: false,
     transports: ['websocket']}
     
