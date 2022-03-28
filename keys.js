7a86d93dfe96bc0b693770508eb0272ac68d4199
Saylu Jalloh <saylujalloh@gmail.com>
2022-03-28 16:37:24 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -4,8 +4,7 @@ export const aws = {
     bucketPost: 'gsa-image-store', 
     bucketProfile: 'gsa-profile-image',
     region: 'us-east-2',
-    accessKey: 'AKIAVEQU6EBCSI4Z437G',
-    secretKey: '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',
+    accessKey: '',
+    secretKey: '',
 }
 
-//arn:aws:iam::353304977477:policy/gsa-bucket-rules
